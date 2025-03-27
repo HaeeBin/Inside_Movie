c83f28346b7527734d8e26682b9ab133b5d07137
Haeebin <goqls4112@gmail.com>
2025-03-27 14:47:53 +0900

    code flake, black, isort check

--- dags/boxoffice_daily_crawling.py
+++ dags/boxoffice_daily_crawling.py
@@ -1,12 +1,13 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.models import Variable     # Airflow의 환경변수 불러오기 위함
-import requests
-import pandas as pd
-from datetime import datetime, timedelta
-from google.cloud import storage
 import os
+from datetime import datetime, timedelta
+
+import pandas as pd
+import requests
+from airflow import DAG
+from airflow.models import Variable  # Airflow의 환경변수 불러오기 위함
+from airflow.operators.python import PythonOperator
 from dotenv import load_dotenv
+from google.cloud import storage
 
 load_dotenv()
 
@@ -17,100 +18,117 @@ BOXOFFICE_API_KEY = Variable.get("BOXOFFICE_API_KEY")
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_APPLICATION_CREDENTIALS
 
+
 def get_date():
-    '''
+    """
     API로 어제 날짜의 데이터까지만 수집 가능하므로 어제 날짜일자를 구하는 함수입니다.
-    '''
+    """
     return (datetime.today() - timedelta(days=1)).strftime("%Y%m%d")
-    
+
+
 def request_daily_api(**kwargs):
-    '''
+    """
     발급받은 key와 조회하고자 하는 날짜 등 정보를 입력하여 요청해 일별 박스오피스 데이터를 가져오는 함수입니다.
-    '''
+    """
     target_date = get_date()
-    
-    base_url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json'
+
+    base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     params = {
-        "key" : BOXOFFICE_API_KEY,
-        "targetDt" : target_date,              # 조회하고자 하는 날짜
+        "key": BOXOFFICE_API_KEY,
+        "targetDt": target_date,  # 조회하고자 하는 날짜
         # "multiMovieYn" : "N",             # 다양성 영화 : Y, 상업 영화 : N (default: 전체)
         # "repNationCd" : "K",              # 한국 영화 : K, 외국 영화 : F (default: 전체)
         # "wideAreaCd" : "0105000000"       # 지역코드 (공통코드 조회 서비스에서 10자리 숫자로 조회된 지역코드)
     }
-    
+
     response = requests.get(base_url, params=params)
-    
+
     if response.status_code == 200:
-        kwargs['ti'].xcom_push(key='daily_box_office_data', value=response.json())
+        kwargs["ti"].xcom_push(key="daily_box_office_data", value=response.json())
     else:
         raise Exception(f"API 요청 실패: {response.status_code}")
 
+
 def parse_daily_boxoffice_data(data):
-    '''
+    """
     가져온 박스오피스 데이터 중 필요한 정보들만 모아서 Dataframe으로 변환하는 함수입니다.
-    '''     
-    box_office_list = data['boxOfficeResult']['dailyBoxOfficeList']
-    
+    """
+    box_office_list = data["boxOfficeResult"]["dailyBoxOfficeList"]
+
     df = pd.DataFrame(box_office_list)
-    
-    df = df[[
-        "rank", "rankInten", "rankOldAndNew",
-        "movieCd", "movieNm", "openDt",
-        "salesAmt", "salesShare", "salesInten", "salesChange", "salesAcc",
-        "audiCnt", "audiInten", "audiChange", "audiAcc",
-        "scrnCnt", "showCnt"
-    ]]
-    
+
+    df = df[
+        [
+            "rank",
+            "rankInten",
+            "rankOldAndNew",
+            "movieCd",
+            "movieNm",
+            "openDt",
+            "salesAmt",
+            "salesShare",
+            "salesInten",
+            "salesChange",
+            "salesAcc",
+            "audiCnt",
+            "audiInten",
+            "audiChange",
+            "audiAcc",
+            "scrnCnt",
+            "showCnt",
+        ]
+    ]
+
     return df
 
+
 def upload_to_gcs(**kwargs):
-    '''
+    """
     DataFrame을 csv로 변환하여 Google Cloud Storage에 업로드하는 함수입니다.
-    '''
+    """
     target_date = get_date()
-    
+
     client = storage.Client()
     bucket = client.bucket(BUCKET_NAME)
-    
+
     # gcs 파일 경로 설정
     gcs_file_path = f"{DAILY_BOXOFFICE_FOLDER}/daily_box_office_{target_date}.csv"
     blob = bucket.blob(gcs_file_path)
-    
+
     # 데이터 받아서 parse_dataframe(data)함수로 변환
     ti = kwargs["ti"]
-    data = ti.xcom_pull(task_ids='request_daily_api', key='daily_box_office_data')
-    
+    data = ti.xcom_pull(task_ids="request_daily_api", key="daily_box_office_data")
+
     df = parse_daily_boxoffice_data(data)
-    
+
     # dataframe을 csv 변환 후 gcs 업로드
-    csv_data = df.to_csv(index=False, encoding='utf-8-sig')
+    csv_data = df.to_csv(index=False, encoding="utf-8-sig")
     blob.upload_from_string(csv_data, content_type="text/csv")
-    
+
     print(f"daily boxOffice 업로드 완료. 날짜 : {target_date}")
+
 
 # dag 설정
 default_args = {
     "start_date": datetime(2025, 3, 20),
-    "retries" : 1,
-    "retry_delay" : timedelta(minutes=10),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=10),
 }
 with DAG(
     dag_id="daily_box_office_crawling",
-    schedule_interval=timedelta(days=1),     # 매일 실행하기 위함
+    schedule_interval=timedelta(days=1),  # 매일 실행하기 위함
     catchup=False,
-    default_args=default_args
+    default_args=default_args,
 ) as dag:
-    
+
     request_daily_api = PythonOperator(
-        task_id='request_daily_api',
+        task_id="request_daily_api",
         python_callable=request_daily_api,
-        provide_context=True
+        provide_context=True,
     )
-    
+
     upload_to_gcs = PythonOperator(
-        task_id='upload_to_gcs',
-        python_callable=upload_to_gcs,
-        provide_context=True
+        task_id="upload_to_gcs", python_callable=upload_to_gcs, provide_context=True
     )
-    
-    request_daily_api >> upload_to_gcs                                                                                                                                                                                                                                                                                                                                                                                                                   
+
+    request_daily_api >> upload_to_gcs
--- dags/boxoffice_daily_region_crawling.py
+++ dags/boxoffice_daily_region_crawling.py
@@ -1,12 +1,13 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.models import Variable     # Airflow의 환경변수 불러오기 위함
-import requests
-import pandas as pd
-from datetime import datetime, timedelta
-from google.cloud import storage
 import os
+from datetime import datetime, timedelta
+
+import pandas as pd
+import requests
+from airflow import DAG
+from airflow.models import Variable  # Airflow의 환경변수 불러오기 위함
+from airflow.operators.python import PythonOperator
 from dotenv import load_dotenv
+from google.cloud import storage
 
 load_dotenv()
 
@@ -17,156 +18,187 @@ BOXOFFICE_API_KEY = Variable.get("BOXOFFICE_API_KEY")
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_APPLICATION_CREDENTIALS
 
+
 def get_date():
-    '''
+    """
     어제 날짜일자를 구하는 함수입니다.
-    '''
+    """
     return (datetime.today() - timedelta(days=1)).strftime("%Y%m%d")
 
+
 def load_region_codes_from_gcs(**kwargs):
-    '''
+    """
     gcs에 저장된 region.csv를 가져와 dataframe으로 변환하는 함수입니다.
-    '''
-    REGION_CODES_FOLDER = os.getenv('REGION_CODES_FOLDER')
-    
+    """
+    REGION_CODES_FOLDER = os.getenv("REGION_CODES_FOLDER")
+
     client = storage.Client()
     bucket = client.bucket(BUCKET_NAME)
-    blob = bucket.blob(f'{REGION_CODES_FOLDER}/region_codes.csv')
-    
+    blob = bucket.blob(f"{REGION_CODES_FOLDER}/region_codes.csv")
+
     csv_data = blob.download_as_text()
     df = pd.read_csv(pd.io.StringIO(csv_data))
-    
-    kwargs["ti"].xcom_push(key="load_region_codes_from_gcs", value=df.to_dict("records"))
-    
+
+    kwargs["ti"].xcom_push(
+        key="load_region_codes_from_gcs", value=df.to_dict("records")
+    )
+
+
 def request_region_api(**kwargs):
-    '''
-    key, 날짜 등의 필수 정보들과 지역 코드를 입력해 특정 지역의 일별 박스오피스 데이터를 가져오는 함수입니다. 
-    '''
+    """
+    key, 날짜 등의 필수 정보들과 지역 코드를 입력해 특정 지역의 일별 박스오피스 데이터를 가져오는 함수입니다.
+    """
     target_date = get_date()
-    
-    region_codes = kwargs["ti"].xcom_pull(task_ids="load_region_codes_from_gcs", key="load_region_codes_from_gcs")
-    
+
+    region_codes = kwargs["ti"].xcom_pull(
+        task_ids="load_region_codes_from_gcs", key="load_region_codes_from_gcs"
+    )
+
     all_data = []
     for region in region_codes:
         region_code = str(region["fullCd"])
         region_name = str(region["korNm"])
-        
-        base_url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json'
+
+        base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
         params = {
-            "key" : BOXOFFICE_API_KEY,
-            "targetDt" : target_date,           # 조회하고자 하는 날짜
-            "wideAreaCd" : region_code,         # 지역코드 (공통코드 조회 서비스에서 10자리 숫자로 조회된 지역코드)
+            "key": BOXOFFICE_API_KEY,
+            "targetDt": target_date,  # 조회하고자 하는 날짜
+            "wideAreaCd": region_code,  # 지역코드 (공통코드 조회 서비스에서 10자리 숫자로 조회된 지역코드)
             # "multiMovieYn" : "N",             # 다양성 영화 : Y, 상업 영화 : N (default: 전체)
             # "repNationCd" : "K",              # 한국 영화 : K, 외국 영화 : F (default: 전체)
         }
-    
+
         response = requests.get(base_url, params=params)
-    
+
         if response.status_code == 200:
             data = response.json()
-            all_data.append({
-                "region_code" : region_code,
-                "region_name" : region_name,
-                "data" : data
-            })
+            all_data.append(
+                {"region_code": region_code, "region_name": region_name, "data": data}
+            )
         else:
             print(f"API 요청 실패: {response.status_code} : {region_name}")
-        
-    kwargs['ti'].xcom_push(key='daily_region_box_office_data', value=all_data)
+
+    kwargs["ti"].xcom_push(key="daily_region_box_office_data", value=all_data)
+
 
 def parse_daily_region_boxoffice_data(**kwargs):
-    '''
+    """
     지역별 박스오피스 데이터를 모아놓은 데이터를 필요한 정보만 남기고 dataframe으로 변환.
-    '''     
-    all_data = kwargs["ti"].xcom_pull(task_ids='request_region_api', key='daily_region_box_office_data')
-    
+    """
+    all_data = kwargs["ti"].xcom_pull(
+        task_ids="request_region_api", key="daily_region_box_office_data"
+    )
+
     all_df = []
-    
+
     for d in all_data:
         region_code = d["region_code"]
         region_name = d["region_name"]
         data = d["data"]
-        
+
         box_office_list = data.get("boxOfficeResult", {}).get("dailyBoxOfficeList", [])
-    
+
         if box_office_list:
             df = pd.DataFrame(box_office_list)
-            
+
             df["region_code"] = region_code
             df["region_name"] = region_name
-            
-            df = df[[
-                "region_code", "region_name",
-                "rank", "rankInten", "rankOldAndNew",
-                "movieCd", "movieNm", "salesAmt", "salesAcc",
-                "audiCnt", "audiAcc", "scrnCnt", "showCnt"
-            ]]
-            
+
+            df = df[
+                [
+                    "region_code",
+                    "region_name",
+                    "rank",
+                    "rankInten",
+                    "rankOldAndNew",
+                    "movieCd",
+                    "movieNm",
+                    "salesAmt",
+                    "salesAcc",
+                    "audiCnt",
+                    "audiAcc",
+                    "scrnCnt",
+                    "showCnt",
+                ]
+            ]
+
             all_df.append(df)
-    
+
     if all_df:
-        result = pd.concat(all_df, ignore_index=True)   # 지역별로 데이터 수집한거 하나의 dataframe으로 합치는 과정
-        kwargs["ti"].xcom_push(key="parse_region_boxoffice_data", value=result.to_json())
+        result = pd.concat(
+            all_df, ignore_index=True
+        )  # 지역별로 데이터 수집한거 하나의 dataframe으로 합치는 과정
+        kwargs["ti"].xcom_push(
+            key="parse_region_boxoffice_data", value=result.to_json()
+        )
+
 
 def upload_to_gcs(**kwargs):
-    '''
+    """
     DataFrame을 csv로 변환하여 Google Cloud Storage에 업로드하는 함수입니다.
-    '''
+    """
     target_date = get_date()
-    
+
     client = storage.Client()
     bucket = client.bucket(BUCKET_NAME)
-    
+
     # gcs 파일 경로 설정
-    gcs_file_path = f"{DAILY_REGION_BOXOFFICE_FOLDER}/daily_region_box_office_{target_date}.csv"
+    gcs_file_path = (
+        f"{DAILY_REGION_BOXOFFICE_FOLDER}/daily_region_box_office_{target_date}.csv"
+    )
     blob = bucket.blob(gcs_file_path)
-    
+
     # parse_daily_region_boxoffice_data 메소드에서 데이터 가져오기
-    data = kwargs["ti"].xcom_pull(task_ids='parse_daily_region_boxoffice_data', key='parse_region_boxoffice_data')
-    
+    data = kwargs["ti"].xcom_pull(
+        task_ids="parse_daily_region_boxoffice_data", key="parse_region_boxoffice_data"
+    )
+
     # dataframe을 csv 변환 후 gcs 업로드
     if data:
         df = pd.read_json(data)
         csv_data = df.to_csv(index=False, encoding="utf-8-sig")
         blob.upload_from_string(csv_data, content_type="text/csv")
-        
+
         print(f"{target_date} region file upload success")
-    
+
 
 # dag 설정
 default_args = {
     "start_date": datetime(2025, 3, 20),
-    "retries" : 1,
-    "retry_delay" : timedelta(minutes=10),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=10),
 }
 with DAG(
     dag_id="daily_region_box_office_crawling",
-    schedule_interval=timedelta(days=1),     # 매일 실행하기 위함
+    schedule_interval=timedelta(days=1),  # 매일 실행하기 위함
     catchup=False,
-    default_args=default_args
+    default_args=default_args,
 ) as dag:
-    
+
     load_region_codes_from_gcs = PythonOperator(
-        task_id='load_region_codes_from_gcs',
+        task_id="load_region_codes_from_gcs",
         python_callable=load_region_codes_from_gcs,
     )
-    
+
     request_region_api = PythonOperator(
-        task_id='request_region_api',
+        task_id="request_region_api",
         python_callable=request_region_api,
-        provide_context=True
+        provide_context=True,
     )
-    
+
     parse_daily_region_boxoffice_data = PythonOperator(
         task_id="parse_daily_region_boxoffice_data",
         python_callable=parse_daily_region_boxoffice_data,
-        provide_context=True
+        provide_context=True,
     )
-    
+
     upload_to_gcs = PythonOperator(
-        task_id='upload_to_gcs',
-        python_callable=upload_to_gcs,
-        provide_context=True
+        task_id="upload_to_gcs", python_callable=upload_to_gcs, provide_context=True
     )
-    
-    load_region_codes_from_gcs >> request_region_api >> parse_daily_region_boxoffice_data >> upload_to_gcs
+
+    (
+        load_region_codes_from_gcs
+        >> request_region_api
+        >> parse_daily_region_boxoffice_data
+        >> upload_to_gcs
+    )
